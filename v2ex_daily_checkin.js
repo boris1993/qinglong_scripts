467624cc11f00b9a336f0fdf1bac775bd52ab8a7
Boris Zhao <boris1993@live.cn>
2022-11-25 11:18:02 +0800

    Update headers

--- v2ex_daily_checkin.js
+++ v2ex_daily_checkin.js
@@ -19,9 +19,9 @@ let headers = {
     Cookie: COOKIE,
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
     Referer: 'https://www.v2ex.com/',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9',
     'accept-encoding': 'gzip, deflate, br',
-    'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+    'accept-language': 'zh-CN,zh;q=0.9',
     'cache-control': 'no-cache',
     'Content-Type': 'text/html;charset=utf-8',
 }
