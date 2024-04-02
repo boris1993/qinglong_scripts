e343b37891419775065d1d33a75030f22ad7c120
Boris Zhao <boris1993@live.cn>
2024-04-02 21:43:17 +0800

    fix: Using dynamic import for Octokit/core

--- github-sync-upstream.js
+++ github-sync-upstream.js
@@ -1,42 +1,46 @@
 // [task_local]
 // 定时同步GitHub上游repo
 // 0 0 * * * github-sync-upstream.js, tag=定时同步GitHub上游repo, enabled=true
-const { Octokit } = require("@octokit/core");
+async function main() {
+    const { Octokit } = await import("@octokit/core");
 
-const githubRepoOwner = process.env.GITHUB_REPO_OWNER;
-if (!githubRepoOwner) {
-    console.error("GitHub repo owner not specified");
-    return;
+    const githubRepoOwner = process.env.GITHUB_REPO_OWNER;
+    if (!githubRepoOwner) {
+        console.error("GitHub repo owner not specified");
+        return;
+    }
+    
+    // Example:
+    // [{"repo": "is-a-dev-register","branch": "main"},{"repo": "logback","branch": "master"}]
+    const repos = process.env.GITHUB_REPOS;
+    if (!repos) {
+        console.error("No repo specified");
+        return;
+    }
+    
+    const githubAccessToken = process.env.GITHUB_TOKEN;
+    if (!githubAccessToken) {
+        console.error("GitHub access token not specified");
+        return;
+    }
+    
+    const octokit = new Octokit({
+        auth: githubAccessToken,
+    });
+    
+    JSON.parse(repos).forEach(async (repository) => {
+        console.log(`Syncing ${githubRepoOwner}/${repository["repo"]}`);
+    
+        const response = await octokit.request(
+            "POST /repos/{owner}/{repo}/merge-upstream",
+            {
+                owner: githubRepoOwner,
+                repo: repository["repo"],
+                branch: repository["branch"]
+            });
+    
+        console.log(`${repository["repo"]} synced, status ${response.status}, response message: ${response.data["message"]}`);
+    });    
 }
 
-// Example:
-// [{"repo": "is-a-dev-register","branch": "main"},{"repo": "logback","branch": "master"}]
-const repos = process.env.GITHUB_REPOS;
-if (!repos) {
-    console.error("No repo specified");
-    return;
-}
-
-const githubAccessToken = process.env.GITHUB_TOKEN;
-if (!githubAccessToken) {
-    console.error("GitHub access token not specified");
-    return;
-}
-
-const octokit = new Octokit({
-    auth: githubAccessToken,
-});
-
-JSON.parse(repos).forEach(async (repository) => {
-    console.log(`Syncing ${githubRepoOwner}/${repository["repo"]}`);
-
-    const response = await octokit.request(
-        "POST /repos/{owner}/{repo}/merge-upstream",
-        {
-            owner: githubRepoOwner,
-            repo: repository["repo"],
-            branch: repository["branch"]
-        });
-
-    console.log(`${repository["repo"]} synced, status ${response.status}, response message: ${response.data["message"]}`);
-});
+main();
